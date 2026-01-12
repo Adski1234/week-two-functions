fed29fdd340566e722d828a0fda236ed7afdaf30
ADAM RICHARDSON <ADAM.RICHARDSON123@GMAIL.COM>
2026-01-12 13:31:26 +0000

    Version 2

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!");
+
+function sayHello() {
+    console.log("Hello from sayHello function!");
+}
+
+
+sayHello();
+sayHello();sayHello();
+sayHello();
+sayHello();
+
+function sayHelloTo(userName)   {
+    console.log("Hello, " + userName + "!");
+}
+
+function greet(greeting, username) {
+    console.log(greeting + ", " + username + "!");
+}
+greet("Good morning", "Charlie");
+greet("Hi", "Dana");
+
+sayHelloTo("Alice");
+sayHelloTo("Bob");
+
